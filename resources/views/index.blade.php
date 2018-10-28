56ed7cad698b591229eb02059ae3c5cdc3d80d80
wuwx <wuweixin@gmail.com>
2018-10-28 14:00:22 +0800

    use card style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,21 @@
 @extends('connect::layouts.master')
 
 @section('content')
-    <h1>{!! config('connect.name') !!}</h1>
+    <div class="card">
+        <div class="card-header">
+            Connect
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Providers</h5>
 
-    <table class="table">
-        @foreach($providers as $provider)
-        <tr>
-            <td>{{ $provider->name }}</td>
-            <td><a href="connect" class="btn btn-primary">Connect</a></td>
-        </tr>
-        @endforeach
-    </table>
+            <table class="table">
+                @foreach($providers as $provider)
+                    <tr>
+                        <td>{{ $provider->name }}</td>
+                        <td><a href="{{ $provider->name }}" class="btn btn-primary">Connect</a></td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
 @stop
