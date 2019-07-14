e16cff7c96f4f3878bfec4507d9698034662a216
wuwx <wuweixin@gmail.com>
2019-07-14 22:28:18 +0800

    Controller Tests

---
+++ tests/Http/Controllers/Admin/ProviderControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Wuwx\LaravelConnect\tests\Http\Controllers\Admin;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProviderControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
